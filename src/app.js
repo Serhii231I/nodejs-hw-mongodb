98960c89bbead4af8081a11c2d8af873581cdeac
Serhii Ivashchenko <Ivashchenkoserezha25@gmail.com>
2025-04-15 23:57:17 +0200

    add hope all

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import path from 'node:path';
+
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
@@ -7,6 +9,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const app = express();
+app.use('/uploads', express.static(path.resolve('src', 'uploads')));
 app.use(cookieParser());
 app.use(routes);
 app.use(cors());
